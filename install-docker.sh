67d6926fb9f5204f4331dd3e880059495450653f
Tom Shafer <tom.shafer@elderresearch.com>
2021-12-06 13:10:44 -0500

    Better working in Docker

--- install-docker.sh
+++ install-docker.sh
@@ -1,8 +1,20 @@
 # Install dotfiles into Docker via VS Code Remote Container.
 # Just cat the files into their (maybe nonexistent) counterparts
+# Usage: install-docker.sh [PATH_TO_DOTFILES: ~/dotfiles]
 
-for df in bash/* git/* python/* hushlogin screenrc; do
+dotpath="~/dotfiles"
+if [[ -n $1 ]]; then
+    dotpath="$1"
+fi
+
+for df in "$dotpath"/{bash/*,git/*,python/*,hushlogin,screenrc}; do
+
     target=".$(basename $df)"
+    echo "$df -> $target"
+
     echo "\n" >> "$target"
     cat "$df" >> "$target"
+
 done
+
+unset dotpath
