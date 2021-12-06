edf65a6fb35de5b572d439ae93f47d070e308b24
Tom Shafer <tom.shafer@elderresearch.com>
2021-12-06 13:29:53 -0500

    PWD

--- install-docker.sh
+++ install-docker.sh
@@ -5,7 +5,7 @@
 # Usage: install-docker.sh [PATH_TO_DOTFILES: ~/dotfiles]
 
 dotpath=$([[ -z $1 ]] && echo ~/dotfiles ||echo "$1")
-
+echo "PWD: $(pwd)"
 for df in $dotpath/{bash/*,git/*,python/*,hushlogin,screenrc}; do
     target=".$(basename $df)"
 
