da393e18ca1e51473e3c45f48264e9c6ab242c4f
Tom Shafer <tom.shafer@elderresearch.com>
2021-12-14 12:41:05 -0500

    Allow skipping Git

--- install-docker.sh
+++ install-docker.sh
@@ -2,9 +2,12 @@
 
 # Install dotfiles into Docker via VS Code Remote Container.
 # Just cat the files to the end of their (maybe nonexistent) counterparts.
+# Usage: install-docker.sh [--no-git]
 
 for df in bash/* git/* python/* hushlogin screenrc; do
     target="$HOME/.$(basename $df)"
+
+    [[ $1 == "--no-git" && $target =~ gitconfig$ ]] && continue
 
     echo "$df -> $target"
     cat "$df" >> "$target"
