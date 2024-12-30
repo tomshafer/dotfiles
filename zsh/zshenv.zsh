37c58cb045435d101da05eadee50a0197ead8a96
Tom Shafer <tom@tshafer.com>
2024-12-29 20:38:40 -0500

    Re-add bat paging

--- zsh/zshenv.zsh
+++ zsh/zshenv.zsh
@@ -1,28 +1,27 @@
 #!/bin/zsh
 
+
 # Environment
 export EDITOR=vim
 export VISUAL="$EDITOR"
 
 
 # Path
-# [[ $(type -t brew) ]] && PATH="$(brew --prefix)/bin:$(brew --prefix)/sbin:$PATH"
-# [[ -d $HOME/bin ]] && PATH="$HOME/bin:$PATH"
-# typeset -U path
-# export PATH="$PATH"
+typeset -U path
 
-# if [[ $(type -t bat) ]]; then
-#   export MANPAGER="sh -c 'col -bx | bat -l man -p --pager=\"less -RF\"'"
-# else
-#   export MANPAGER="less -X"
-# fi
 
-# Avoid issues with `gpg` as installed via Homebrew.
-# https://stackoverflow.com/a/42265848
+# Homebrew defaults
+if command -v brew > /dev/null; then
+    export HOMEBREW_NO_ENV_HINTS=1
+    PATH="$(brew --prefix)/bin:$(brew --prefix)/sbin:$PATH"
+fi
+
+
+# Avoid issues with `gpg` as installed via Homebrew [1]
 GPG_TTY=$(tty)
 export GPG_TTY
 
-# # Homebrew defaults
-# if [[ $(type -t brew) ]]; then
-#   export HOMEBREW_NO_ENV_HINTS=1
-# fi
+
+# References
+# --------------------------------------------------------------------
+# [1]: https://stackoverflow.com/a/42265848
--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -29,11 +29,22 @@ fi
 source "$DF/completions.zsh"
 
 
+# Misc
+# --------------------------------------------------------------------
+# Use bat for paging [5]
+if command -v bat > /dev/null; then
+    export MANPAGER="sh -c 'col -bx | bat -l man -p --pager=\"less -RF\"'"
+else 
+    export MANPAGER="less -X"
+fi
+
+
 # References
 # --------------------------------------------------------------------
 # [1]: https://dev.to/rossijonas/how-to-set-up-history-based-autocompletion-in-zsh-k7o
 # [2]: https://superuser.com/a/1078653
 # [3]: https://unix.stackexchange.com/a/115431
 # [4]: https://stackoverflow.com/a/54618022
+# [5]: https://github.com/sharkdp/bat?tab=readme-ov-file#man
 
 unset DF
