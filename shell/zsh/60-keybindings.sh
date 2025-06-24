b871950a75adb0ca5aef478be8d5acf19afa6092
Tom Shafer <tom.shafer@elderresearch.com>
2025-06-24 09:19:56 -0400

    Almost-working configs

--- shell/zsh/60-keybindings.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env zsh
-#  Keyboard bindings and other options
-
-# Emacs mode (like I've used with bash)
-bindkey -e                      
-
-# Forward delete with Delete: https://superuser.com/a/1078653
-bindkey "^[[3~" delete-char     
