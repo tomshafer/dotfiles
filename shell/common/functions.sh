f3736a4bed6e2ec6f7c1991b06aa14af4d8c41ea
Tom Shafer <tom@tshafer.com>
2025-06-30 21:22:48 -0400

    Add venv() activator

--- shell/common/functions.sh
+++ shell/common/functions.sh
@@ -12,3 +12,44 @@ mkd() {
   [ $# != 1 ] && echo >&2 "Usage: mkd DIRECTORY" && return 1
   mkdir -p "$1" && cd "$1" && echo >&2 "mkdir \"$1\""
 }
+
+######################################################################
+# Find and activate a virtual env in the directory tree
+######################################################################
+venv() {
+  # If a virtual env is set, deactivate it
+  if [[ -n $VIRTUAL_ENV ]]; then
+    if command -v deactivate >/dev/null; then
+      echo "Deactivated $VIRTUAL_ENV"
+      deactivate
+    fi
+  fi
+
+  local here parent dr
+  here="$(pwd)"
+
+  while :; do
+    # Try to fine a venv in the directory
+    for dr in .venv .env venv env; do
+      if [[ -d $here/$dr ]]; then
+        echo "Activated   $here/$dr"
+        # shellcheck disable=SC1090
+        source "$here"/"$dr"/bin/activate
+        echo "python:     $(which python)"
+        echo "pip:        $(which pip)"
+        return
+      fi
+    done
+
+    # If not, loop again with the parent
+    parent=$(dirname "$here")
+
+    # Break when the parent is repeated
+    if [[ $parent == "$here" ]]; then
+      break
+    fi
+
+    here="$parent"
+  done
+  echo "No venv was located" >&2
+}
