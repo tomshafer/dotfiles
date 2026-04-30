d3443ebffb2c9b499df3bd74e1b2ce968d796634
Tom Shafer <tom@tshafer.com>
2026-04-30 07:02:18 -0400

    Fix man pager

--- shell/common/exports.sh
+++ shell/common/exports.sh
@@ -41,9 +41,7 @@ fi
 
 # Pagers ---------------------------------------------------
 
-# TODO: Add termcap colors if we don't have bat
-
-# Don't reset the screen
+# Don't reset the screen, keep ANSI colors, and exit if output fits.
 export LESS="-RF"
 export LESSHISTFILE="-"  # Don't store a less history file
 export PAGER="less"
@@ -53,15 +51,10 @@ export MANPAGER="less -R"
 export LESS_TERMCAP_mb=$'\033[1;31m'
 export LESS_TERMCAP_md=$'\033[1;36m'
 export LESS_TERMCAP_me=$'\033[0m'
-export LESS_TERMCAP_so=$'\033[01;44;33m'
+export LESS_TERMCAP_so=$'\033[1;33;44m'
 export LESS_TERMCAP_se=$'\033[0m'
 export LESS_TERMCAP_us=$'\033[1;32m'
 export LESS_TERMCAP_ue=$'\033[0m'
-
-# Use bat: https://github.com/sharkdp/bat?tab=readme-ov-file#man
-if command -v bat >/dev/null 2>&1; then
-    export MANPAGER="sh -c 'col -bx | bat -l man -p --pager=\"less\"'"
-fi
 
 # Colors ---------------------------------------------------
 
